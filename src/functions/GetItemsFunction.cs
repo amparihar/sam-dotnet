b40cabf978ce50d1c0b14cf5b242860aa6bc2465
Amit Parihar <aparihar@corp.lpl.com>
2020-09-14 11:39:23 +0530

    Modified functions to remove using block

--- src/functions/GetItemsFunction.cs
+++ src/functions/GetItemsFunction.cs
@@ -17,13 +17,13 @@ namespace Lambda.Functions
 {
     public class GetItemsFunction
     {
-        private DynamoDBService _dbService;
+        private readonly DynamoDBService _dbService;
 
         private readonly string _tableName = Environment.GetEnvironmentVariable("ITEM_TABLE_NAME");
 
         public GetItemsFunction()
         {
-            
+            _dbService = new DynamoDBService();
         }
 
         [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -32,29 +32,26 @@ namespace Lambda.Functions
             var requestModel = JsonConvert.DeserializeObject<GetItemRequest>(request.Body);
             try
             {
-                _dbService = new DynamoDBService();
-                using (var client = _dbService.DbClient)
+                var client = _dbService.DbClient;
+                Table ItemTable;
+                var loadTableSuccess = false;
+                loadTableSuccess = Table.TryLoadTable(
+                    client,
+                    _tableName,
+                    DynamoDBEntryConversion.V2, out ItemTable);
+                if (loadTableSuccess)
                 {
-                    Table ItemTable;
-                    var loadTableSuccess = false;
-                    loadTableSuccess = Table.TryLoadTable(
-                        client,
-                        _tableName,
-                        DynamoDBEntryConversion.V2, out ItemTable);
-                    if (loadTableSuccess)
-                    {
-                        var filter = new QueryFilter("id", QueryOperator.Equal, requestModel.Id);
-                        filter.AddCondition("key", QueryOperator.BeginsWith, requestModel.Key);
+                    var filter = new QueryFilter("id", QueryOperator.Equal, requestModel.Id);
+                    filter.AddCondition("key", QueryOperator.BeginsWith, requestModel.Key);
 
-                        var documentResponse = await ItemTable.Query(filter).GetRemainingAsync();
-                        // TO DO: Use Extention Method 
-                        // Modify mapper functions as extension methods
-                        var queryResponse = Mapper.ToItemResponse(documentResponse);
+                    var documentResponse = await ItemTable.Query(filter).GetRemainingAsync();
+                    // TO DO: Use Extention Method 
+                    // Modify mapper functions as extension methods
+                    var queryResponse = Mapper.ToItemResponse(documentResponse);
 
-                        return ResponseHandler.ProcessResponse(HttpStatusCode.OK, JsonConvert.SerializeObject(queryResponse));
-                    }
-                    return ResponseHandler.ProcessResponse(HttpStatusCode.NotFound, $"Resource: {_tableName} not found");
+                    return ResponseHandler.ProcessResponse(HttpStatusCode.OK, JsonConvert.SerializeObject(queryResponse));
                 }
+                return ResponseHandler.ProcessResponse(HttpStatusCode.NotFound, $"Resource: {_tableName} not found");
             }
             catch (AmazonDynamoDBException ddbEx)
             {
--- src/functions/SaveItemFunction.cs
+++ src/functions/SaveItemFunction.cs
@@ -17,13 +17,13 @@ namespace Lambda.Functions
 {
     public class SaveItemFunction
     {
-        private DynamoDBService _dbService;
+        private readonly DynamoDBService _dbService;
 
         private readonly string _tableName = Environment.GetEnvironmentVariable("ITEM_TABLE_NAME");
 
         public SaveItemFunction()
         {
-
+            _dbService = new DynamoDBService();
         }
 
         [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -32,24 +32,21 @@ namespace Lambda.Functions
             var requestModel = JsonConvert.DeserializeObject<SaveItemRequest>(request.Body);
             try
             {
-                _dbService = new DynamoDBService();
-                using (var client = _dbService.DbClient)
+                var client = _dbService.DbClient;
+                Table ItemTable;
+                var loadTableSuccess = false;
+                loadTableSuccess = Table.TryLoadTable(
+                    client,
+                    _tableName,
+                    DynamoDBEntryConversion.V2, out ItemTable);
+                if (loadTableSuccess)
                 {
-                    Table ItemTable;
-                    var loadTableSuccess = false;
-                    loadTableSuccess = Table.TryLoadTable(
-                        client,
-                        _tableName,
-                        DynamoDBEntryConversion.V2, out ItemTable);
-                    if (loadTableSuccess)
-                    {
-                        // TO DO: Use Extension method
-                        var newItem = Mapper.ToSaveItemDocumentModel(requestModel);
-                        await ItemTable.PutItemAsync(newItem);
-                        return ResponseHandler.ProcessResponse(HttpStatusCode.Created, JsonConvert.SerializeObject(requestModel));
-                    }
-                    return ResponseHandler.ProcessResponse(HttpStatusCode.NotFound, $"Resource: {_tableName} not found");
+                    // TO DO: Use Extension method
+                    var newItem = Mapper.ToSaveItemDocumentModel(requestModel);
+                    await ItemTable.PutItemAsync(newItem);
+                    return ResponseHandler.ProcessResponse(HttpStatusCode.Created, JsonConvert.SerializeObject(requestModel));
                 }
+                return ResponseHandler.ProcessResponse(HttpStatusCode.NotFound, $"Resource: {_tableName} not found");
             }
             catch (AmazonDynamoDBException ddbEx)
             {
